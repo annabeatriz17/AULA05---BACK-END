4642e1baf017714323daab91b68d759db97bf1ab
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-08-29 17:13:53 -0300

    refactor: alter filmes routes structure

--- src/server.js
+++ src/server.js
@@ -8,68 +8,8 @@ const port = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
-const guloseimas = [
-    {
-        id: 1,
-        nome:"Trufa",
-        preco:8.50
-    },
-    {
-        id:2,
-        nome:"Sorvete",
-        preco:10.5
-    },
-    {
-        id:3,
-        nome:"Trento de nutella",
-        preco:3.50
-    }
-]
-
-const filmesMarcantes = [
-    {
-        id: 1001,
-        nome:"Velozes e Furiosos",
-        genero: "Ação",
-        emCartaz: false
-    },
-    {
-        id:1002,
-        nome:"É assim que acaba",
-        genero: "Romance",
-        emCartaz: true
-    },
-    {
-        id:1003,
-        nome:"Meu Malvado Favorito 4",
-        genero: "Animação",
-        emCartaz: true
-    }
-]
-
 app.get("/",(req, res) => {
     return res.status(200).send({ message: "Hello,World!" })
-});
-
-app.get("/doces",(req, res) => {
-    return res.status(200).send(guloseimas);
-});
-app.post("/doces",(req, res) => {
-    const{nome,preco} = req.body;
-
-const novoDoce = {
-    id:guloseimas.length + 1,
-    nome: nome,
-    preco: preco,
-};
-
-guloseimas.push(novoDoce);
-
-    return res.status(201).send(guloseimas);
-});
-
-app.get("/filmes",(req, res) => {
-    return res.status(200).send(filmesMarcantes);
 });
 
 app.listen(port, () => {
