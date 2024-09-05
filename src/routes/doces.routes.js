6eb3ae6d2a11554e598bb1650c5f462f02908224
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-09-05 09:57:44 -0300

    feat: create get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { response, Router } from "express";
 
 const docesRoutes = Router();
 
@@ -20,10 +20,12 @@ const guloseimas = [
     }
 ];
 
+//Rota para buscar todos os elementos do array guloseimas
 docesRoutes.get("/",(req, res) => {
     return res.status(200).send(guloseimas);
 });
 
+//Rota para criar nova guloseimas
 docesRoutes.post("/",(req, res) => {
     const{nome,preco} = req.body;
     
@@ -32,10 +34,26 @@ docesRoutes.post("/",(req, res) => {
         nome: nome,
         preco: preco,
     };
-    
+
     guloseimas.push(novoDoce);
-    
+
     return res.status(201).send(guloseimas);
     });
 
-    export default docesRoutes;
+    //Rota para buscar um elemento específico do array guloseimas
+
+    docesRoutes.get("/:id", (req,res) => {
+        const { id } = req.params
+        const guloseima = guloseimas.find((doce) => doce.id === Number(id)
+);
+
+console.log(guloseima);
+
+    if (!guloseima) {
+        return res.status(404).send({ message: "Guloseima não encontrada!"});
+    }
+        
+        return res.status(200).send(guloseima)
+})
+
+    export default docesRoutes
